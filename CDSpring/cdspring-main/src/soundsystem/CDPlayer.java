314ff14973dfcbe561a82f607f0289a4d84be8f3
延昊南 <yhnkb@hotmail.com>
2017-01-17 21:27:23 +0800

    自动装配

--- CDSpring/cdspring-main/src/soundsystem/CDPlayer.java
+++ CDSpring/cdspring-main/src/soundsystem/CDPlayer.java
@@ -1,10 +1,12 @@
 package soundsystem;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Vegeta on 1/17/17.
  */
+@Component
 public class CDPlayer implements MediaPlayer {
     private CompactDisc cd;
 
--- CDSpring/cdspring-main/test/CDPlayerTest.java
+++ CDSpring/cdspring-main/test/CDPlayerTest.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import soundsystem.CompactDisc;
+import soundsystem.MediaPlayer;
 
 import static junit.framework.TestCase.assertNotNull;
 
@@ -15,11 +16,19 @@ import static junit.framework.TestCase.assertNotNull;
 public class CDPlayerTest {
 
     @Autowired
+    MediaPlayer player;
+
+    @Autowired
     CompactDisc cd;
 
     @Test
     public void testCdShouldNotBeNull() {
         assertNotNull(cd);
         cd.play();
+    }
+
+    @Test
+    public void testMediaPlay() {
+        player.play();
     }
 }
