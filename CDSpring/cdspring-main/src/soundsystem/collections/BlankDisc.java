1693a477f47c2989f811ef62be7283b4344e9a61
延昊南 <yhnkb@hotmail.com>
2017-01-17 21:58:47 +0800

    手动装配-xml

---
+++ CDSpring/cdspring-main/src/soundsystem/collections/BlankDisc.java
@@ -0,0 +1,29 @@
+package soundsystem.collections;
+
+import soundsystem.CompactDisc;
+
+import java.util.List;
+
+/**
+ * Created by Vegeta on 1/17/17.
+ */
+public class BlankDisc implements CompactDisc {
+
+    private String title;
+    private String artist;
+    private List<String> tracks;
+
+    public BlankDisc(String title, String artist, List<String> tracks) {
+        this.title = title;
+        this.artist = artist;
+        this.tracks = tracks;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("Playing " + title + " by " + artist);
+        for (String track : tracks) {
+            System.out.println("-Track: " + track);
+        }
+    }
+}
