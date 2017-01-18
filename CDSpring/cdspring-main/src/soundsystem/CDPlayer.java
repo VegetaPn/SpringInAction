d65f08bfd1a6efc18e354d98d60b32b9173d8f94
延昊南 <yhnkb@hotmail.com>
2017-01-18 21:35:23 +0800

    设置属性

--- CDSpring/cdspring-main/src/soundsystem/CDPlayer.java
+++ CDSpring/cdspring-main/src/soundsystem/CDPlayer.java
@@ -6,17 +6,22 @@ import org.springframework.stereotype.Component;
 /**
  * Created by Vegeta on 1/17/17.
  */
-@Component
+//@Component
 public class CDPlayer implements MediaPlayer {
     private CompactDisc cd;
 
-    @Autowired
-    public CDPlayer(CompactDisc cd) {
-        this.cd = cd;
-    }
+//    @Autowired
+//    public CDPlayer(CompactDisc cd) {
+//        this.cd = cd;
+//    }
 
     @Override
     public void play() {
         cd.play();
+    }
+
+    @Autowired
+    public void setCd(CompactDisc cd) {
+        this.cd = cd;
     }
 }
--- CDSpring/cdspring-main/src/soundsystem/collections/BlankDisc.java
+++ CDSpring/cdspring-main/src/soundsystem/collections/BlankDisc.java
@@ -13,11 +13,11 @@ public class BlankDisc implements CompactDisc {
     private String artist;
     private List<String> tracks;
 
-    public BlankDisc(String title, String artist, List<String> tracks) {
-        this.title = title;
-        this.artist = artist;
-        this.tracks = tracks;
-    }
+//    public BlankDisc(String title, String artist, List<String> tracks) {
+//        this.title = title;
+//        this.artist = artist;
+//        this.tracks = tracks;
+//    }
 
     @Override
     public void play() {
@@ -26,4 +26,17 @@ public class BlankDisc implements CompactDisc {
             System.out.println("-Track: " + track);
         }
     }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public void setTracks(List<String> tracks) {
+        this.tracks = tracks;
+    }
+
 }
