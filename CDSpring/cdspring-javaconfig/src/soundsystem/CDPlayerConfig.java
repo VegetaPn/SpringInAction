0b168f8f077f2d3b898c5b8f1ed2e89cc3582f48
延昊南 <yhnkb@hotmail.com>
2017-01-19 22:12:17 +0800

    javaconfig

---
+++ CDSpring/cdspring-javaconfig/src/soundsystem/CDPlayerConfig.java
@@ -0,0 +1,38 @@
+package soundsystem;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Vegeta on 1/19/17.
+ */
+@Configuration
+public class CDPlayerConfig {
+    @Bean
+    public CompactDisc sgtPeppers() {
+        return new SgtPeppers();
+    }
+
+    @Bean
+    public CompactDisc randomBeatlesCD() {
+        int choice = (int) Math.floor(Math.random() * 3);
+        System.out.println("choice: " + choice);
+        if (choice == 0) {
+            return new SgtPeppers();
+        } else if (choice == 1) {
+            return new SgtPeppers();
+        } else {
+            return new SgtPeppers();
+        }
+    }
+
+    @Bean
+    public CDPlayer cdPlayer() {
+        return new CDPlayer(sgtPeppers());
+    }
+
+    @Bean
+    public CDPlayer anotherCDPlayer() {
+        return new CDPlayer(sgtPeppers());
+    }
+}
